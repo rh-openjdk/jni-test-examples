6d94619c3080d5f0282b48dfc26ab3340b40dc10
Jiri Vanek <jvanek@redhat.com>
2025-08-20 11:14:31 +0200

    Excluded testMessageHdrMultipleControl on ppc64le
    apepared  3.1.7 with  unable to be fixed in upstream

--- jni/jnr-posix.sh
+++ jni/jnr-posix.sh
@@ -8,26 +8,32 @@ sub=jnr-posix
 rm -rf $sub
 mkdir  $sub
 pushd  $sub
+PATCH=false
 if [ "x$JDK_MAJOR" == "x" -o "0$JDK_MAJOR" -ge 17 ] ; then
   JNR_LIVE_PROJECTS="jnr-posix:3.1.20"
    export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED"
+  if [ "x$OS_ARCH" == "xppc64le" ] ; then
+    PATCH=true
+  fi
 elif [ "0$JDK_MAJOR" -ge 11 ] ; then
   JNR_LIVE_PROJECTS="jnr-posix:3.1.7"
+  if [ "x$OS_ARCH" == "xppc64le" ] ; then
+    PATCH=true
+  fi
 else
   JNR_LIVE_PROJECTS="jnr-posix:3.0.58"
 fi
 for x in $JNR_LIVE_PROJECTS ; do
   project=`echo $x | sed "s/:.*//"`
   version=`echo $x | sed "s/.*://"`
-  if [ "$project" == "jnr-x86asm" ] ; then 
-    wget https://github.com/jnr/$project/archive/$version.tar.gz
-    tar -xf $version.tar.gz
-    pushd $project-$version
-  else
-    wget https://github.com/jnr/$project/archive/$project-$version.tar.gz
-    tar -xf $project-$version.tar.gz
-    pushd $project-$project-$version
-  fi
+  wget https://github.com/jnr/$project/archive/$project-$version.tar.gz
+  tar -xf $project-$version.tar.gz
+  pushd $project-$project-$version
+    if [ "x$PATCH" == "xtrue" ] ; then
+      # Ignore testMessageHdrMultipleControl on ppc64le
+      # https://github.com/jnr/jnr-posix/issues/178
+      sed -e '/testMessageHdrMultipleControl/i @Ignore'  -e '/import org.junit.Test/a import org.junit.Ignore;' -i src/test/java/jnr/posix/LinuxPOSIXTest.java
+    fi
     if [ "x$PURGE_MVN" == "xtrue" ] ; then  $EX_MVN $MVOPTS dependency:purge-local-repository -DreResolve=false ; fi
     $EX_MVN $MVOPTS -Dmaven.javadoc.skip=true clean install
   popd
